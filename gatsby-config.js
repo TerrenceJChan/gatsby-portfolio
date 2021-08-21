37b92d5afc058c08083a280b85b87ec0dcc7e9db
Terrence Chan <terrencechan.writer@gmail.com>
2021-08-21 01:07:35 -0400

    Initializing styles

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,9 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
-    siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
+    title: `Terrence Chan`,
+    description: `Proud code monkey! Welcome to my personal site!`,
+    author: `Terrence`,
+    siteUrl: `https://terrencejchan.com/`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
